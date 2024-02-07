e6fbdef6501f9ae39158baa014400f4e292210c6
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2024-02-07 21:16:22 +0900

    openunion

--- sutra-extensible-effects/core/Data/OpenUnion.hs
+++ sutra-extensible-effects/core/Data/OpenUnion.hs
@@ -2,11 +2,12 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE ViewPatterns #-}
@@ -16,10 +17,19 @@ module Data.OpenUnion (
   inj,
   prj,
   pattern U0',
+  decomp,
+  pattern U0,
+  pattern U1,
   Member,
+  SetMember,
+  type (<::),
+  weaken,
 ) where
 
 import Data.Kind (Type)
+import Data.List (deleteFirstsBy)
+import Data.Traversable (for)
+import GHC.Base (Constraint)
 import GHC.TypeLits (
   ErrorMessage (
     ShowType,
@@ -69,6 +79,77 @@ pattern U0' h <- (prj -> Just h)
   where
     U0' h = inj h
 
+{- | Explicit type -level equality condition is a deleteFirstsBy
+hack to eliminate the type annotation in the trivial case,
+such as @run (runReader () get)@
+
+There is no ambiguity when finding instances for
+@Member t (a ': b ': r)@, which the second instance is selected.
+
+the only case we have to concerned about is @Member t '[s]@
+But, in this case, values of definition is the same (if present),
+and the first one is chosen according to GHC User Manual, since
+the latter one is incoherent. This is the optimal choice.
+-}
+instance {-# OVERLAPPING #-} (t ~ s) => Member t '[s] where
+  {-# INLINE inj #-}
+  {-# INLINE prj #-}
+  prj (Union _ x) = Just (unsafeCoerce x)
+  inj = Union 0
+
+-- Note that if it weren't for us wanting to use the specialized instance above
+-- we wouldn't need the INCOHERENT pragma below
+-- TODO: consider impact of disabling specialization
+instance {-# INCOHERENT #-} (FindElem t r) => Member t r where
+  {-# INLINE inj #-}
+  {-# INLINE prj #-}
+  inj = inj' (unP (elemNo :: P t r))
+  prj = prj' (unP (elemNo :: P t r))
+
+{- | A useful operator for reducing boilerplate in signatures.
+
+The following lines are equivalent.
+
+@
+(Member (Exc e) r, Member (State s) r) => ...
+[ Exc e, State s] <:: r =>
+@
+-}
+type family (<::) (ms :: [Type -> Type]) r where
+  (<::) '[] r = (() :: Constraint)
+  (<::) (m ': ms) r = (Member m r, (<::) ms r)
+
+{-# INLINE [2] decomp #-}
+
+-- | Orthogonal decomposition of the union: head and the rest.
+decomp :: Union (t ': r) v -> Either (Union r v) (t v)
+decomp (Union 0 v) = Right $ unsafeCoerce v
+decomp (Union n v) = Left $ Union (n - 1) v
+
+{- | Some helpful pattern synonyms.
+U0 : the first element of the union
+-}
+pattern U0 :: t v -> Union (t ': r) v
+pattern U0 h <- (decomp -> Right h)
+  where
+    U0 h = inj h
+
+-- | U1 : everithing excluding the first element of the union.
+pattern U1 t <- (decomp -> Left t)
+  where
+    U1 t = weaken t
+
+{-# COMPLETE U0, U1 #-}
+
+-- Specialized version
+{-# RULES "decomp/singleton" decomp = decomp0 #-}
+{-# INLINE decomp0 #-}
+decomp0 :: Union '[t] v -> Either (Union '[] v) (t v)
+decomp0 (Union _ v) = Right $ unsafeCoerce v
+
+weaken :: Union r w -> Union (any ': r) w
+weaken (Union n v) = Union (n + 1) v
+
 {- | Find the index of an element in a type-level list.
 The element must exist
 This is essentially a compile-time computation.
@@ -95,3 +176,26 @@ instance
   FindElem t '[]
   where
   elemNo = error "unreachable"
+
+{- | Using overlapping instances here is OK since this class is private to this
+module
+-}
+class EQU (a :: k) (b :: k) p | a b -> p
+
+instance EQU a a 'True
+instance {-# OVERLAPPABLE #-} (p ~ 'False) => EQU a b p
+
+-- | This class is used for emulating monad transformers
+class (Member t r) => SetMember (tag :: k -> Type -> Type) (t :: Type -> Type) r | tag r -> t
+
+instance (EQU t1 t2 p, MemberU' p tag t1 (t2 ': r)) => SetMember tag t1 (t2 ': r)
+
+class
+  (Member t r) =>
+  MemberU' (f :: Bool) (tag :: k -> Type -> Type) (t :: Type -> Type) r
+    | tag r -> t
+
+instance MemberU' 'True tag (tag e) (tag e ': r)
+instance
+  (Member t (t' ': r), SetMember tag t r) =>
+  MemberU' 'False tag t (t' ': r)
